acc2383760c922867b372a17cd1607cf9e2ddc1a
kh8 <reczek.krzysztof@gmail.com>
2017-04-05 21:33:16 +0200

    Compiler - Snippet factory done and covered with tests

--- os7-compiler/src/main/java/com/h8/compiler/core/s7/generator/SnippetFactory.java
+++ os7-compiler/src/main/java/com/h8/compiler/core/s7/generator/SnippetFactory.java
@@ -5,10 +5,45 @@ import com.h8.compiler.core.s7.snippets.S7DynamicSnippet;
 public class SnippetFactory {
     public String create(S7DynamicSnippet snippet, SnippetParameter params) {
         String s = snippet.getSnippet();
+        return replaceParameters(s, params);
+    }
+
+    private String replaceParameters(String s, SnippetParameter params) {
         for (String key : params.keySet()) {
             SnippetParameter sp = params.get(key);
-            s = s.replace("$" + key, sp.getValue());
+            s = sp.isNested() ?
+                    replaceNestedParameters(s, key, sp) :
+                    replaceSingleParameter(s, key, sp);
         }
         return s;
+    }
+
+    private String replaceNestedParameters(String s, String key, SnippetParameter params) {
+        String prefix = "@" + key + "[";
+        Integer prefixIdx = s.indexOf(prefix);
+
+        String suffix = "]";
+        Integer suffixIdx = s.indexOf(suffix, prefixIdx);
+
+        String nested = s.substring(prefixIdx + prefix.length(), suffixIdx);
+        String snippets = collectNestedSnippets(nested, params);
+
+        return s.replace(prefix + nested + suffix, snippets);
+    }
+
+    private String collectNestedSnippets(String nested, SnippetParameter params) {
+        String s = "";
+        for (String key : params.keySet()) {
+            if (!s.isEmpty()) {
+                s += System.getProperty("line.separator");
+            }
+            SnippetParameter sp = params.get(key);
+            s += replaceParameters(nested, sp);
+        }
+        return s;
+    }
+
+    private String replaceSingleParameter(String s, String key, SnippetParameter param) {
+        return s.replace("$" + key, param.getValue());
     }
 }
--- os7-compiler/src/test/java/com/h8/compiler/core/s7/generator/SnippetFactoryTest.java
+++ os7-compiler/src/test/java/com/h8/compiler/core/s7/generator/SnippetFactoryTest.java
@@ -10,6 +10,7 @@ public class SnippetFactoryTest {
     public void snippetWithSingleParameterCanBeCreated() throws Exception {
         SnippetParameter sp = SnippetParameter.nestedParameters();
         sp.put("NAME", SnippetParameter.singleParameters("TEST_NAME"));
+
         String result = new SnippetFactory().create(S7DynamicSnippet.TYPE, sp);
         assertTrue(result.contains("TYPE \"TEST_NAME\""));
     }
@@ -19,8 +20,33 @@ public class SnippetFactoryTest {
         SnippetParameter sp = SnippetParameter.nestedParameters();
         sp.put("NAME", SnippetParameter.singleParameters("TEST_NAME"));
         sp.put("VERSION", SnippetParameter.singleParameters("TEST_VERSION"));
+
         String result = new SnippetFactory().create(S7DynamicSnippet.TYPE, sp);
         assertTrue(result.contains("TYPE \"TEST_NAME\""));
         assertTrue(result.contains("VERSION : TEST_VERSION"));
+    }
+
+    @Test
+    public void snippetWithNestedParametersCanBeCreated() throws Exception {
+        SnippetParameter p1 = SnippetParameter.nestedParameters();
+        p1.put("NAME", SnippetParameter.singleParameters("TEST_NAME1"));
+        p1.put("TYPE", SnippetParameter.singleParameters("TEST_TYPE1"));
+        p1.put("COMMENT", SnippetParameter.singleParameters("TEST_COMMENT1"));
+
+        SnippetParameter p2 = SnippetParameter.nestedParameters();
+        p2.put("NAME", SnippetParameter.singleParameters("TEST_NAME2"));
+        p2.put("TYPE", SnippetParameter.singleParameters("TEST_TYPE2"));
+        p2.put("COMMENT", SnippetParameter.singleParameters("TEST_COMMENT2"));
+
+        SnippetParameter parameters = SnippetParameter.nestedParameters();
+        parameters.put("1", p1);
+        parameters.put("2", p2);
+
+        SnippetParameter sp = SnippetParameter.nestedParameters();
+        sp.put("PARAMETERS", parameters);
+
+        String result = new SnippetFactory().create(S7DynamicSnippet.TYPE, sp);
+        assertTrue(result.contains("TEST_NAME1 : TEST_TYPE1; //TEST_COMMENT1"));
+        assertTrue(result.contains("TEST_NAME2 : TEST_TYPE2; //TEST_COMMENT2"));
     }
 }
