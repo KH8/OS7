4874d5638136945bedcc8e511a7ab3c0a7a69d0a
kh8 <reczek.krzysztof@gmail.com>
2017-05-25 23:17:16 +0200

    Compiler - Fields extracted to S7Type init

--- os7-compiler/src/main/java/com/h8/compiler/core/processors/generator/ClassTypesBuilder.java
+++ os7-compiler/src/main/java/com/h8/compiler/core/processors/generator/ClassTypesBuilder.java
@@ -2,7 +2,9 @@ package com.h8.compiler.core.processors.generator;
 
 import com.h8.compiler.core.context.CompilationContext;
 import com.h8.compiler.core.context.components.ClassContext;
+import com.h8.compiler.core.context.components.FieldContext;
 import com.h8.compiler.core.s7.generator.components.S7CodeComponents;
+import com.h8.compiler.core.s7.generator.components.S7Parameter;
 import com.h8.compiler.core.s7.generator.components.S7Type;
 import com.h8.compiler.core.s7.snippets.S7DynamicSnippet;
 import com.h8.compiler.core.s7.snippets.SnippetFactory;
@@ -16,21 +18,42 @@ public class ClassTypesBuilder extends OutputFileWriter {
     protected String getContent() {
         String result = "";
         for (ClassContext c : context.getClasses().values()) {
-            result += getTypeFromClassContext(c);
-            result += getContentSeparator();
+            if (checkIfClassShouldBeProcessed(c)) {
+                result += getTypeFromClassContext(c);
+                result += getContentSeparator();
+            }
         }
         return result;
+    }
+
+    private boolean checkIfClassShouldBeProcessed(ClassContext c) {
+        return !getTypeName(c).isEmpty();
     }
 
     private String getTypeFromClassContext(ClassContext c) {
         String name = getTypeName(c);
         String version = getVersion();
-        S7Type sb = new S7Type(name, version, new S7CodeComponents<>());
+        S7Type sb = new S7Type(name, version, getTypeParameters(c));
         return new SnippetFactory().create(S7DynamicSnippet.TYPE, sb.toSnippetParameter());
     }
 
+    private S7CodeComponents<S7Parameter> getTypeParameters(ClassContext c) {
+        S7CodeComponents<S7Parameter> parameters = new S7CodeComponents<>();
+        for (FieldContext fc : c.getFields().values()) {
+            String name = getName(fc.getF().getName());
+            S7Type type = new S7Type("INT", "", new S7CodeComponents<>());
+            S7Parameter p = new S7Parameter(name, type,
+                    "Class field " + fc.getF().getName() + " of type: " + fc.getF().getType().getSimpleName());
+            parameters.add(p);
+        }
+        return parameters;
+    }
+
     private String getTypeName(ClassContext c) {
-        String className = c.getC().getSimpleName();
-        return className.replaceAll("(.)(\\p{Upper})", "$1_$2").toUpperCase();
+        return getName(c.getC().getSimpleName());
+    }
+
+    private String getName(String name) {
+        return name.replaceAll("(.)(\\p{Upper})", "$1_$2").toUpperCase();
     }
 }
